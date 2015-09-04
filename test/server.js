af6ab45022c92b8ccfd24b7250e70ceadcf2d78b
Kevin Neff <kvnneff@gmail.com>
2015-09-04 10:38:02 -0700

    more tests

--- test/server.js
+++ test/server.js
@@ -18,29 +18,50 @@ describe('App', function () {
     })
   })
 
-  afterEach(function (done) {
-    server.io.httpServer.on('close', function () {
-      done()
-    })
-    server.destroy()
-    socket.disconnect()
-  })
-
   describe('App()', function () {
-    it('creates a new player on connect', function () {
+    it('creates a new player when user connects', function () {
       assert(server.Players.length === 1)
+      server.destroy()
+      socket.disconnect()
     })
   })
   describe('App#listen', function () {
-    it('listens for incoming connections')
+    it('creates a socket', function () {
+      assert(server.io)
+      server.destroy()
+      socket.disconnect()
+    })
   })
   describe('App#stats', function () {
-    it('returns a collection of server stats')
+    it('returns a collection of server stats', function () {
+      var stats = server.stats()
+      assert(stats.games)
+      assert(stats.players)
+      assert(stats.players.length === 1)
+      server.destroy()
+      socket.disconnect()
+    })
   })
   describe('App#sendStats', function () {
-    it('broadcasts a collection of server stats')
+    it('broadcasts a collection of server stats', function (done) {
+      socket.on('stats', function (stats) {
+        assert(stats.games)
+        assert(stats.players)
+        assert(stats.players.length === 1)
+        server.destroy()
+        socket.disconnect()
+        done()
+      })
+    })
   })
   describe('App#destroy', function () {
-    it('shuts down the server')
+    it('shuts down the server', function (done) {
+      server.io.httpServer.on('close', function () {
+        assert.equal(server.interval, 0)
+        assert.equal(socket.connected, false)
+        done()
+      })
+      server.destroy()
+    })
   })
 })
