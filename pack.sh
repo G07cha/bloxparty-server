924ec892a48dcf68b14a275b4697c91818f43d41
Kevin Neff <kvnneff@gmail.com>
2015-09-05 11:14:00 -0700

    fix deploy script

--- pack.sh
+++ pack.sh
@@ -1,9 +1,9 @@
 # $1 - first parameter is a name of the archive.
 # zip all files except of node_modules (modules binaries could depend on hardware)
 zip $1 * -r --exclude=*node_modules*
-mkdir bin
-mv $1 bin/$1
+mkdir build
+mv $1 build/$1
 echo -e "\n>ls -l:\n"
 ls -l
-echo -e "\n>ls bin/ -l:\n"
-ls bin/ -l
+echo -e "\n>ls -l build/:\n"
+ls -l build/
